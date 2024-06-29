cab84414a2cb0efc1c934d2cc8aba51edd3f0e45
Aingaran Thirunadarajah <email@aingaran.com>
2024-06-29 13:14:07 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger")
-  const navLinks = document.querySelector(".nav-links")
+const submitForm = async (event) => {
+  event.preventDefault()
+  const form = event.target
+  const data = new FormData(form)
+  const name = data.get("name")
+  const email = data.get("email")
+  const phone = data.get("phone")
+  const message = data.get("message")
 
-  hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("active")
+  const payload = {
+    name,
+    email,
+    phone,
+    message,
+  }
+
+  const response = await fetch("https://6gb.ca/api/techfox", {
+    method: "POST",
+    body: JSON.stringify(payload),
+    // nocors
+    cors: "no-cors",
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
   })
-})
+
+  if (response.ok) {
+    form.reset()
+    alert("Thank you for your message. We will get back to you soon.")
+  } else {
+    alert("Something went wrong. Please try again later.")
+  }
+}
